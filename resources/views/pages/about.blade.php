5cc250f883c89965525ac5182329bbab77062141
Divya <007divyachawla@users.noreply.github.com>
2020-05-09 15:50:37 +0530

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,5 @@
 
 @section('content')
     <h1>About</h1>
-    <p>Nothing much really, the series will be containing real-life (!CTF/Generic) challenges I come across daily while pentesting applications.</p>
-    <p>You can find me <a href="https://twitter.com/syed__umar" target="_blank">@syed__umar</a> and the challenges @
-    	<a href="https://pentestlabs.gitbook.io" target="_blank">PentestLabs</a>. Also, these challenges were inspired by <a href="https://owasp-skf.gitbook.io" target="_blank">OWASP SKF</a>.</p>
+    <p>This is the Ctf, in which you have to hijack the admin and get the flag</p>
 @endsection
